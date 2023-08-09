a77f774001fbcc70baeb544e0e9bb6afbfd6fd82
followsclosely <mlavwilson@gmail.com>
2023-08-08 22:14:13 -0400

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ export class Bucket {
 })
 export class App {
   name = 'Angular';
-  showId : boolean = true;
+  showId : boolean = false;
 
   buckets : Bucket[] = [
     new Bucket(1978, [
@@ -76,7 +76,48 @@ export class App {
       new Shield("2586px5",     "https://img.bricklink.com/ItemImage/PT/9/2586px5.t1.png"),
       new Shield("3846px5",     "https://img.bricklink.com/ItemImage/PT/9/3846px5.t1.png")]
     ),
-
+    new Bucket(2001, [
+      new Shield("2586ph1",     "https://img.bricklink.com/ItemImage/PT/9/2586ph1.t1.png")]
+    ),
+    new Bucket(2002, [
+      new Shield("2586ph2",     "https://img.bricklink.com/ItemImage/PT/11/2586ph2.t1.png"),
+      new Shield("2586px13",    "https://img.bricklink.com/ItemImage/PT/9/2586px13.t1.png")]
+    ),
+    new Bucket(2003, [
+      new Shield("2586",        "https://img.bricklink.com/ItemImage/PT/86/2586.t1.png"),
+      new Shield("2586px12 ",   "https://img.bricklink.com/ItemImage/PT/11/2586px12.t1.png")]
+    ),
+    new Bucket(2004, [
+      new Shield("48494",       "https://img.bricklink.com/ItemImage/PT/99/48494.t1.png"),
+      new Shield("48494pb01",   "https://img.bricklink.com/ItemImage/PT/99/48494pb01.t1.png"),
+      new Shield("48494pb02",   "https://img.bricklink.com/ItemImage/PT/99/48494pb02.t1.png"),
+      new Shield("48494pb03",   "https://img.bricklink.com/ItemImage/PT/99/48494pb03.t1.png"),
+      new Shield("48494pb04",   "https://img.bricklink.com/ItemImage/PT/99/48494pb04.t1.png"),
+      new Shield("48494pb05",   "https://img.bricklink.com/ItemImage/PT/99/48494pb05.t1.png")]
+    ),
+    new Bucket(2005, [
+      new Shield("2586px15",    "https://img.bricklink.com/ItemImage/PT/86/2586px15.t1.png"),
+      new Shield("2586px16",    "https://img.bricklink.com/ItemImage/PT/59/2586px16.t1.png"),
+      new Shield("2586px6",     "https://img.bricklink.com/ItemImage/PT/86/2586px6.t1.png"),
+      new Shield("3846pb015",   "https://img.bricklink.com/ItemImage/PT/99/3846pb015.t1.png"),
+      new Shield("3846pb016",   "https://img.bricklink.com/ItemImage/PT/99/3846pb016.t1.png"),
+      new Shield("3846pb017",   "https://img.bricklink.com/ItemImage/PT/99/3846pb017.t1.png"),
+      new Shield("3846pb018",   "https://img.bricklink.com/ItemImage/PT/99/3846pb018.t1.png"),
+      new Shield("3846pb019",   "https://img.bricklink.com/ItemImage/PT/86/3846pb019.t1.png"),
+      new Shield("3846pb025",   "https://img.bricklink.com/ItemImage/PT/3/3846pb025.t1.png"),
+      new Shield("48494pb06",   "https://img.bricklink.com/ItemImage/PT/99/48494pb06.t1.png")]
+    ),
+    new Bucket(2006, [
+      new Shield("2586pb001",   "https://img.bricklink.com/ItemImage/PT/86/2586pb001.t1.png"),
+      new Shield("2586px18",    "https://img.bricklink.com/ItemImage/PT/86/2586px18.t1.png"),
+      new Shield("2586px7",     "https://img.bricklink.com/ItemImage/PT/86/2586px7.t1.png"),
+      new Shield("3846pb020",   "https://img.bricklink.com/ItemImage/PT/86/3846pb020.t1.png"),
+      new Shield("3846pb021",   "https://img.bricklink.com/ItemImage/PT/115/3846pb021.t1.png"),
+      new Shield("3846pb022",   "https://img.bricklink.com/ItemImage/PT/115/3846pb022.t1.png"),
+      new Shield("3846pb023",   "https://img.bricklink.com/ItemImage/PT/86/3846pb023.t1.png"),
+      new Shield("3846pb026",   "https://img.bricklink.com/ItemImage/PT/86/3846pb026.t1.png"),
+      new Shield("48494pb07",   "https://img.bricklink.com/ItemImage/PT/99/48494pb07.t1.png")]
+    ),
   ];
 
 
